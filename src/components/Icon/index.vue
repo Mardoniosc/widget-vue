bc9b1bd64c450760f8b40472946ed85a890f2924
Mardonio Costa <mardonio@live.com>
2022-07-12 16:37:25 -0400

    Criando botão flutuante que abre widget pt1

---
+++ src/components/Icon/index.vue
@@ -0,0 +1,19 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template lang="">
+  <component :is="name" v-bind="$props"></component>
+</template>
+<script>
+
+import Loading from './Loading.vue'
+import Copy from './Copy.vue'
+import ChevronDown from './ChevronDown.vue'
+import Chat from './Chat.vue'
+
+export default {
+
+  components: { Loading, Copy, ChevronDown, Chat },
+  props: {
+    name: { type: String, required: true }
+  }
+}
+</script>
---
+++ src/views/Widget/Box.vue
@@ -0,0 +1,9 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <h1>Box</h1>
+</template>
+<script>
+export default {
+
+}
+</script>
