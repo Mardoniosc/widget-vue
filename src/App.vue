d41b0fea38b095b14b7a72450b89ebd24e851018
Mardonio Costa <mardonio@live.com>
2022-07-12 15:40:40 -0400

    Configurando playground

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <h1 class="text-4xl">Widget</h1>
+  <widget />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Widget from './views/Widget/index.vue'
 
 export default defineComponent({
-
+  components: { Widget }
 })
 </script>
---
+++ src/views/Widget/index.vue
@@ -0,0 +1,11 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="fixed bottom-0 right-0">
+    <h1>Widget</h1>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
